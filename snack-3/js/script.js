cc800f59812bb83e49c5376ef4bc998f5376cefb
enricopic85 <enricodev85@gmail.com>
2021-12-15 15:04:53 +0100

    terzo snack

---
+++ snack-3/js/script.js
@@ -0,0 +1,12 @@
+//Stampa le potenze di 2 fino a 1000.
+
+let esp=0;
+let potenza=2;
+let risultato=0;
+while (risultato <= 1000) {
+    risultato=potenza ** esp;
+    console.log(risultato);
+    esp++;
+}
+   
+    
